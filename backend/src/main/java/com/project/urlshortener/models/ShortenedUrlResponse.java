5c0a968c4a07969dca74b07647d682c13ff0a19f
Amitrajit Bose <amitrajitbose9@gmail.com>
2020-09-06 14:15:10 +0530

    Fix issue #1, move APISpec to README, edit properties

--- backend/src/main/java/com/project/urlshortener/models/ShortenedUrlResponse.java
+++ backend/src/main/java/com/project/urlshortener/models/ShortenedUrlResponse.java
@@ -4,10 +4,8 @@ import java.util.Date;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
-import lombok.Setter;
 
 @Getter
-@Setter
 @NoArgsConstructor
 @AllArgsConstructor
 public class ShortenedUrlResponse {

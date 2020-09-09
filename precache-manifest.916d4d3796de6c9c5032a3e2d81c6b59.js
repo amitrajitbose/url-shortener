1fe4cf67d3897f989ed8e7d0f3f3a63e61b9ae81
Pankaj Gupta <pankajgupta221b@gmail.com>
2020-09-09 23:30:27 +0530

    Updates

---
+++ precache-manifest.916d4d3796de6c9c5032a3e2d81c6b59.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "39836f38f7cd12ca6df07bb338f309e1",
+    "url": "/url-shortener/index.html"
+  },
+  {
+    "revision": "2e182106042e3250bf52",
+    "url": "/url-shortener/static/css/main.4c157bf4.chunk.css"
+  },
+  {
+    "revision": "eeacbc5da468f446abb0",
+    "url": "/url-shortener/static/js/2.b4c17eb6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/url-shortener/static/js/2.b4c17eb6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2e182106042e3250bf52",
+    "url": "/url-shortener/static/js/main.7f5ca177.chunk.js"
+  },
+  {
+    "revision": "77802c44dcdd9bf07477",
+    "url": "/url-shortener/static/js/runtime-main.7cab408b.js"
+  }
+]);
